38b74c6fca1f27196f8a5ffe5832375e9c883f7a
Jeremie Dimino <jdimino@janestreet.com>
2013-02-19 10:23:45 -0500

    109.10.00

--- lib/pa_pipebang.ml
+++ lib/pa_pipebang.ml
@@ -3,10 +3,10 @@ let f =
   object (self)
      inherit Ast.map as super
      method expr = function
-       | Ast.ExApp(_loc,Ast.ExApp(_,Ast.ExId(_,Ast.IdLid(_,"|!")),x),y)  ->
+       | Ast.ExApp(_loc,Ast.ExApp(_,Ast.ExId(_,Ast.IdLid(_,("|!" | "|>"))),x),y)  ->
          <:expr<$self#expr y$ $self#expr x$>>
-       | Ast.ExId(_loc,Ast.IdLid(_,"|!")) ->
-         Loc.raise _loc (Failure "|! must be applied to two arguments")
+       | Ast.ExId(_loc,Ast.IdLid(_,("|!" | "|>" as s))) ->
+         Loc.raise _loc (Failure (s ^ " must be applied to two arguments"))
        | e -> super#expr e
   end in
 AstFilters.register_str_item_filter f#str_item
